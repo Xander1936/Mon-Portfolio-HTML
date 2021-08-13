2b5eabe413d7bf66e5a3f84065e82cf25fd9704c
Alexandre Massoda <alexandrehervemassodamambeleck@gmail.com>
2021-08-13 01:09:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function(){
+  const scrollTop = window.pageTOffset || this.document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top="-50px";
+  } else {
+    navbar.style.top="0";
+  }
+  lastScrollTop = scrollTop;
+
+});
+
+// TYPED
+var typed = new Typed('.typed', {
+  strings: ["Hello everyone, my name is Alexandre Massoda. I am a technology and computer enthusiast with an atypical profile. After a career in which I discovered several professional environments and practiced more than 03 different jobs, I decided to throw myself fully into learning code, for nearly 18 months; I have been training full-time since April 2020 at OpenClassrooms and self-taught in front-end languages such as CSS, Javascript and its React Framework in order to become a developer. More than a professional future, I discovered a passion, that of coding."],
+  typeSpeed: 20,
+});
+  
+// COMPTEUR LIVE
+let compteur = 0;
+
+$(window).scroll(function() {
+
+  const top = $('.counter').offset().top - window.innerHeight;
+
+  if (compteur == 0 && $(window).scrollTop() > top) {
+    $('.counter-value').each(function() {
+      let $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum : countTo
+      },
+      {
+        duration: 10000,
+        easing: 'swing',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+        }
+      });
+    });
+    compteur = 1;
+  }
+});
+
+//AOS
+AOS.init();
